ba9f415b40c68d7beabdb8d62c51a72ba581eb3b
John Suen <johnnie.suen@gmail.com>
2018-07-15 20:31:33 +1000

    Add customer edit view

--- Vidly2/ViewModels/CustomerFormViewModel.cs
+++ Vidly2/ViewModels/CustomerFormViewModel.cs
@@ -3,7 +3,7 @@ using Vidly2.Models;
 
 namespace Vidly2.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> Membershiptypes { get; set; }
         public Customer Customer { get; set; }
